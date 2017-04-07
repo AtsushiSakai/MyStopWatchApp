1df60cf30f61b61dfeaa5ad4b90b10935132f378
AtsushiSakai <asakai.amsl+github@gmail.com>
2017-04-06 22:32:59 -0700

    add count up

--- MyStopWatchApp/ViewController.swift
+++ MyStopWatchApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    var startTime: TimeInterval? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func update(){
+        //print(Date.timeIntervalSinceReferenceDate)
+        if let startTime = self.startTime {
+            let t = Date.timeIntervalSinceReferenceDate-startTime
+            print(t)
+        }
+    }
 
+    @IBAction func timerStart(_ sender: Any) {
+        startTime = Date.timeIntervalSinceReferenceDate
+        Timer.scheduledTimer(
+            timeInterval: 0.01,
+            target: self,
+            selector: #selector(self.update),
+            userInfo: nil,
+            repeats: true)
+    }
 
+    @IBAction func timerStop(_ sender: Any) {
+    }
+
+    @IBAction func timerReset(_ sender: Any) {
+    }
+    
 }
 
